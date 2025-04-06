9df8b2e78463afc456f20c4197df45f586c92f2b
wilsonliu2 <wilsonliu202@gmail.com>
2025-04-05 20:35:37 -0400

    add cron.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import "dotenv/config";
-
+import job from "./lib/cron.js";
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
