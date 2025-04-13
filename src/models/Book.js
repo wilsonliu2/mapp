ea3e512ac75b60b37b373dc1f357c13c0ba1166f
wilsonliu2 <wilsonliu202@gmail.com>
2025-04-12 20:42:13 -0400

    fix

--- src/models/Book.js
+++ src/models/Book.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 const bookSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    author: { type: String, required: true },
-    description: { type: String, required: true },
+    caption: { type: String, required: true },
+    rating: { type: Number, required: true },
+    image: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true }
 );
