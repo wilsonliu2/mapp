c512e2a9452d15dc1b0730d667513d65e5d7c718
wilsonliu2 <wilsonliu202@gmail.com>
2025-04-17 16:14:46 -0400

    .

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import uploadAiRoute from "./routes/uploadAiRoute.js";
+import aiRoutes from "./routes/aiRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/upload", uploadAiRoute);
-
+app.use("/api/ai", aiRoutes);
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectDB();
